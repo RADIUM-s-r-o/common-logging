27cff9c14e19e8dffdf33d411da0313c3cad9760
Lukáš Vlček <vlcek.lucas@gmail.com>
2026-08-13 14:04:34 +0200

    Download msbuild

--- build-radium-package.cmd
+++ build-radium-package.cmd
@@ -4,7 +4,7 @@ setlocal
 set "ROOT=%~dp0"
 if "%PACKAGE_VERSION%"=="" set "PACKAGE_VERSION=3.4.1-radium.1"
 if "%LOG4NET_DLL%"=="" set "LOG4NET_DLL=%ROOT%packages\log4net.3.3.1\lib\net462\log4net.dll"
-if "%FRAMEWORK_PATH%"=="" set "FRAMEWORK_PATH=%ROOT%packages\Microsoft.NETFramework.ReferenceAssemblies.1.0.3\build\.NETFramework\v4.0"
+if "%FRAMEWORK_PATH%"=="" set "FRAMEWORK_PATH=%ROOT%packages\Microsoft.NETFramework.ReferenceAssemblies.net40.1.0.3\build\.NETFramework\v4.0"
 
 if not exist "%LOG4NET_DLL%" if "%LOG4NET_DLL%"=="%ROOT%packages\log4net.3.3.1\lib\net462\log4net.dll" (
   echo Restoring log4net 3.3.1...
@@ -12,9 +12,9 @@ if not exist "%LOG4NET_DLL%" if "%LOG4NET_DLL%"=="%ROOT%packages\log4net.3.3.1\l
   if errorlevel 1 exit /b 1
 )
 
-if not exist "%FRAMEWORK_PATH%" if "%FRAMEWORK_PATH%"=="%ROOT%packages\Microsoft.NETFramework.ReferenceAssemblies.1.0.3\build\.NETFramework\v4.0" (
+if not exist "%FRAMEWORK_PATH%" if "%FRAMEWORK_PATH%"=="%ROOT%packages\Microsoft.NETFramework.ReferenceAssemblies.net40.1.0.3\build\.NETFramework\v4.0" (
   echo Restoring .NET Framework 4.0 reference assemblies...
-  "%ROOT%tools\nuget\NuGet.exe" install Microsoft.NETFramework.ReferenceAssemblies -Version 1.0.3 -OutputDirectory "%ROOT%packages" -NonInteractive
+  "%ROOT%tools\nuget\NuGet.exe" install Microsoft.NETFramework.ReferenceAssemblies.net40 -Version 1.0.3 -OutputDirectory "%ROOT%packages" -NonInteractive
   if errorlevel 1 exit /b 1
 )
 
